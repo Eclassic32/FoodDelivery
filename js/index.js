a52e72ba1bcec7d3d8a1d7056d7a65c23ba8e649
Eclassic32 <eclassic32004@gmail.com>
2023-02-15 21:06:29 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const jsonURL = "https://raw.githubusercontent.com/Eclassic32/FoodDelivery/master/js/sushi.json?token=GHSAT0AAAAAAB62IANSQXJVD3GX43JI3BFQY7M3UIA";
+
+$.getJSON(jsonURL, function(sushidata){
+    console.log(sushidata)
+    sushidata.forEach(place => {
+        for (let i = 0; i < place.sushi.length; i++) {
+            const item = place.sushi[i];
+            var sushiCard = `<div class="col-3 my-3">
+                                <div class="card">
+                                    <img src="assets/sushi/${place.company}/${i}.jpg" class="card-img-top" alt="">
+                                    <div class="card-body">
+                                        <h4 class="card-title">${item.name}</h5>
+                                        <p class="card-text">${item.desc}</p>
+                                        <h5 id="amount" class="card-text">${item.amount} шт.</h4>
+                                        <h5 id="price" class="card-text">${item.price} ₸</h4>
+                                        <a href="item.html?item=${i}&comp=${place.company}" class="btn btn-info">Заказать</a>
+                                    </div>
+                                </div>
+                            </div>`;
+            $('#allSushi').append(sushiCard);
+            
+        }
+    });
+});
---
+++ js/map.js
@@ -0,0 +1,69 @@
+function initMap() {
+    var startAddress = "Kabanbay Batyr St 320, Almaty";
+    var endAddress = "Bts \"Bayzak\" Abay Ave 52в, Almaty";
+  
+    var geocoder = new google.maps.Geocoder();
+  geocoder.geocode({address: startAddress}, function(results, status) {
+    if (status === 'OK') {
+      var startPoint = results[0].geometry.location;
+      geocoder.geocode({address: endAddress}, function(results, status) {
+        if (status === 'OK') {
+          var endPoint = results[0].geometry.location;
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 10,
+            center: startPoint,
+            mapTypeControl: false,
+            streetViewControl: false,
+            fullscreenControl: false,
+            styles: mapTheme.night
+          });
+
+          var startMarker = new google.maps.Marker({
+            position: startPoint,
+            map: map,
+            label: {
+              text: "\ue57a",
+              fontFamily: "Material Icons",
+              color: "#ffffff",
+              fontSize: "16px",
+            },
+          });
+
+          var endMarker = new google.maps.Marker({
+            position: endPoint,
+            map: map,
+            label: {
+              text: "\ue88a",
+              fontFamily: "Material Icons",
+              color: "#ffffff",
+              fontSize: "16px",
+            },
+          });
+
+          var directionsService = new google.maps.DirectionsService();
+          var directionsRenderer = new google.maps.DirectionsRenderer({
+            map: map,
+            suppressMarkers: true,
+            polylineOptions: {
+              strokeColor: '#FF0000',
+              strokeOpacity: 0.8,
+              strokeWeight: 5
+            }
+          });
+
+          var request = {
+            origin: startPoint,
+            destination: endPoint,
+            travelMode: 'DRIVING'
+          };
+
+          directionsService.route(request, function(result, status) {
+            if (status === 'OK') {
+              directionsRenderer.setDirections(result);
+            }
+          });
+        }
+      });
+    }
+  });
+}
